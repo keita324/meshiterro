334d66796addf83404130544f43fa5bdf537af78
keita324 <tempura0324@gmail.com>
2021-10-18 05:25:25 +0000

    aiueo

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1fdaa919c591688c1425988a3ef050dcb38534bacf51a7af0cc8b0ee6e12b016d413ce5defe42e5b7b196c421311ae91d5b402ffcd8394005ba683b085490e65'
